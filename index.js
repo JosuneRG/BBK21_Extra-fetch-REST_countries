67820dc6f70c51706a9453c84a2aaeb562c39a30
JosuneRG <josunerg94@gmail.com>
2025-05-01 13:45:05 +0200

    cambios realizados

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-
 const countriesNode = document.getElementById("countries");
+const selectContinent = document.getElementById("continentSelect");
+
+let allCountries = []; // Guardamos todos los países aquí
 
 const cardTemplate = function (country) {
   const flag = country?.flags?.png || 'https://via.placeholder.com/200x100?text=No+Flag';
@@ -11,13 +13,23 @@ const cardTemplate = function (country) {
           </div>`;
 };
 
+// Función para mostrar los países (todos o filtrados)
+function mostrarPaises(lista) {
+  countriesNode.innerHTML = ""; // limpiamos
+  lista.forEach(country => {
+    countriesNode.innerHTML += cardTemplate(country);
+  });
+}
 
+// Llamamos a la API y guardamos todos los países
 fetch(`https://restcountries.com/v3.1/all`)
   .then(response => response.json())
   .then(countries => {
-    countries.forEach(country => {
-      countriesNode.innerHTML += cardTemplate(country);
-    });
+    allCountries = countries;1
+    mostrarPaises(allCountries); // Mostrar todos al inicio
+    // countries.forEach(country => {
+    //   countriesNode.innerHTML += cardTemplate(country);
+    // });
   })
   .catch(error => {
     console.error("Error al obtener los países:", error);
@@ -25,3 +37,14 @@ fetch(`https://restcountries.com/v3.1/all`)
   });
 
  
+// Escuchamos el cambio del filtro de continente
+selectContinent.addEventListener("change", () => {
+  const continenteSeleccionado = selectContinent.value;
+
+  if (continenteSeleccionado === "all") {
+    mostrarPaises(allCountries);
+  } else {
+    const filtrados = allCountries.filter(pais => pais.continents && pais.continents.includes(continenteSeleccionado));
+    mostrarPaises(filtrados);
+  }
+});
